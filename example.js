898ed663397b792fe4c996e5135c0b663db94f38
Sudhan <sudhanplayz@gmail.com>
2021-06-03 05:46:27 +0000

    done ;-;

--- example.js
+++ example.js
@@ -2,11 +2,11 @@ require("discordjs-activity")
 const { Client } = require("discord.js")
 const client = new Client()
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
     if(message.channel.author.bot)return
     if(message.content === "yt"){
         let VoiceChannel = client.channels.cache.get("Voice Channel ID")
-        let Invite = VoiceChannel.activity.invite("755600276941176913")
+        let Invite = await VoiceChannel.activityInvite("755600276941176913")//any application id here
         message.channel.send("https://discord.com/invite/"+Invite.code)
     }
 })
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 //Single File package xD
+const { Structures } = require('discord.js');
+const fetch = require("node-fetch")
+
+Structures.extend('VoiceChannel', VoiceChannel => {
+  class EpicVoiceChannel extends VoiceChannel {
+    constructor(client, data) {
+      super(client, data);
+    }
+
+    activityInvite(InviteID){
+        return new Promise(res => {
+            let fetched = fetch(`https://discord.com/api/v8/channels/${this.id}/invites`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                    max_age: 86400,
+                    max_uses: 0,
+                    target_application_id: InviteID,
+                    target_type: 2,
+                    temporary: false,
+                    validate: null
+                }),
+                headers: {
+                    "Authorization": `Bot ${this.client.token}`,
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json())
+            res(fetched)
+        })
+    }
+  }
+  return EpicVoiceChannel;
+});
