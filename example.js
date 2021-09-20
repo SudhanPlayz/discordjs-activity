e07276baaec1e009498fd5a9f36820e35cce7ccd
Uğur <ugurpekesensteam@gmail.com>
2021-09-20 09:44:57 +0300

    Update example.js

--- example.js
+++ example.js
@@ -6,7 +6,7 @@ client.on("message", async (message) => {
     if(message.author.bot)return
     if(message.content === "youtube"){
         let VoiceChannel = client.channels.cache.get("Voice Channel ID")//Voice Channel ID
-        let Invite = await VoiceChannel.activityInvite("755600276941176913")//Application Name [youtube_together, fishington, chess_in_the_park, betrayal, poker_night, chess_in_the_park_dev]
+        let Invite = await VoiceChannel.activityInvite("youtube_together")//Application Name [youtube_together, fishington, chess_in_the_park, betrayal, poker_night, chess_in_the_park_dev]
         if(Invite) {
             message.channel.send("https://discord.com/invite/"+Invite.code)// send's invite link in the channel
         }
